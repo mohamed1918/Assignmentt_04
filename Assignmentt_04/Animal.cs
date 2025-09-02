a8a4964b01063d36c32721bf1446e922c29d487a
mohamed1918 <memoohossam2004@gmail.com>
2025-09-02 14:43:22 +0300

    demooo

---
+++ Assignmentt_04/Animal.cs
@@ -0,0 +1,17 @@
+﻿#region TPT Animal
+public abstract class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class Dog : Animal
+{
+    public string Breed { get; set; }
+}
+
+public class Cat : Animal
+{
+    public string Color { get; set; }
+}
+#endregion
